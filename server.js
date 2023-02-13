3b52d46a77804151820147a4ba7a53a56df73ed5
Rachit1313 <Rachitchawla33@gmail.com>
2023-02-13 01:48:56 -0500

    updated header and new routes

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 /*********************************************************************************
-* WEB322 – Assignment 02
+* WEB322 – Assignment 03
 * I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part
 * of this assignment has been copied manually or electronically from any other source
 * (including 3rd party web sites) or distributed to other students.
 *
-* Name: Rachit Chawla Student ID: 162759211 Date: 29th January,2023
+* Name: Rachit Chawla Student ID: 162759211 Date: 13th February,2023
 *
 * Cyclic Web App URL: https://itchy-red-millipede.cyclic.app
 *
